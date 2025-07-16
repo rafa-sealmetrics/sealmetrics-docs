b1fea8e3585ad3288602618dfa037c0af7d287ed
rafa-sealmetrics <rafa@sealmetrics.com>
2025-07-16 18:10:03 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   organizationName: 'sealmetrics',
   projectName: 'sealmetrics-docs',
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   i18n: {
