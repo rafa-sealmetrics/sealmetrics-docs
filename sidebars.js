71a8b3f889442648875fe2fe2273b9edb4d06b4b
rafa-sealmetrics <rafa@sealmetrics.com>
2025-07-17 17:53:41 +0200

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -4,6 +4,55 @@ const sidebars = {
     'getting-started',
     {
       type: 'category',
+      label: 'First Steps',
+      items: [
+        'first-steps/index',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Features',
+      items: [
+        'features/index',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Platform',
+      items: [
+        'platform/index',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Metrics Insights',
+      items: [
+        'metrics-insights/index',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'API Documentation',
+      items: [
+        'api-documentation/index',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Legal',
+      items: [
+        'legal/index',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Company',
+      items: [
+        'company/index',
+      ],
+    },
+    {
+      type: 'category',
       label: 'Quick Start',
       items: [
         'quick-start/installation',
